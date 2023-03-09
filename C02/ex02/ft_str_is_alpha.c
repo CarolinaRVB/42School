4596e3b2f746e6ca9843e4f86cc6be1103397a6e
Carolina Bastos <carolinavbastos@hotmail.com>
2023-03-09 22:35:12 +0000

    c02

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (('a' < str[i] && str[i] < 'z') || ( 'A' < str[i] && str[i] < 'Z'))
+		{
+			str[i] = '1';	
+		}
+		else
+		{
+			str[i] = '0';
+		}
+		i++;
+	}
+	return *str;
+}
+
+int	main()
+{
+	char	str[9] = "eh#j#$gvg";
+
+	ft_str_is_alpha(str);
+	printf("%s", str);
+}
+
